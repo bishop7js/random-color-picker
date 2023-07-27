9d8b54211ed906b6e51a46baca4e07737676245a
bishop7js <sulakshithait@gmail.com>
2023-07-27 12:34:28 +0530

    Add colors

--- src/components/CardListItem.js
+++ src/components/CardListItem.js
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card } from "@mui/material";
 import CircleIcon from "@mui/icons-material/Circle";
 import { Height } from "@mui/icons-material";
 
 const CardListItem = (color) => {
-  const { colorCode, id } = color.color
+  const [rgbColor, setRgbColor] = useState();
+  const { colorCode, id } = color.color;
+
+  const changeColor = () => {
+    // "rgb(228, 127, 255)"
+
+    let randomRgbColor1 = parseInt(Math.random() * 100);
+    let randomRgbColor2 = parseInt(Math.random() * 200);
+    let randomRgbColor3 = parseInt(Math.random() * 10);
+
+
+    setRgbColor(
+      `rgb(${randomRgbColor1}, ${randomRgbColor2}, ${randomRgbColor3})`
+    );
+  };
 
   return (
     <div>
       <Card>
         <div>{id}</div>
-        <div>
+        <div onClick={() => changeColor()}>
           <CircleIcon
             style={{
-              color: colorCode,
+              color: rgbColor ? rgbColor : colorCode,
               height: "100px",
               width: "100px",
             }}
